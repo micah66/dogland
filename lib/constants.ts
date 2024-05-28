853f211493695e7c513996a88138b9a3c5ed5170
micah66 <micahgordon66@gmail.com>
2024-05-28 20:24:41 +0300

    add map for links to render navigation links

---
+++ lib/constants.ts
@@ -0,0 +1,16 @@
+import Icons from '@/components/shared/Icon'
+
+export const linkMap = {
+  '/': { icon: Icons.Home, label: 'Home' },
+  '/about': { icon: Icons.About, label: 'About' },
+  '/gallery': { icon: Icons.ImagesSquare, label: 'Gallery' },
+  '/kenneling': { icon: Icons.Garage, label: 'Kenneling' },
+  '/training': { icon: Icons.Barbell, label: 'Training' },
+  '/grooming': { icon: Icons.HairDryer, label: 'Grooming' },
+  '/store': { icon: Icons.Storefront, label: 'Store' },
+  '/sign-in': { icon: Icons.SignIn, label: 'Sign In / Up' },
+  '/sign-out': { icon: Icons.SignOut, label: 'Sign Out' },
+  '/settings': { icon: Icons.Cog, label: 'Settings' },
+}
+
+export type TLink = keyof typeof linkMap
