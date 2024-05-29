b7cbbf2be3f19ab4bda693629ef9a8e87abcef3e
Micah Gordon <35034217+micah66@users.noreply.github.com>
2024-05-29 13:38:20 +0300

    Feature authorization (#4)
    * update route from sign-in to sign-up
    
    * add clerk to handle app auth
    
    * remove Link from NavItem and update NavBar
    
    We are removing the Link from within the NavItem, allowing for slots on the NavBar which to not navigated to new URLs. This is the case for items such as 'sign out'

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,8 @@
+import { Component } from '@/types/react'
+import { SignIn } from '@clerk/nextjs'
+
+const SignInPage: Component = () => {
+  return <SignIn />
+}
+
+export default SignInPage
--- components/NavItem.tsx
+++ components/NavItem.tsx
@@ -2,23 +2,24 @@ import { Component } from '@/types/react'
 import Link, { LinkProps } from 'next/link'
 
 import { cn } from '@/lib/utils'
+import { TLink, linkMap } from '@/lib/constants'
 
 type NavItemProps = {
-  icon: JSX.Element
-  label: string
-} & LinkProps
+  link: TLink
+}
 
-const NavItem: Component<NavItemProps> = ({ icon, label, href }) => {
+const NavItem: Component<NavItemProps> = ({ link }) => {
+  const { icon: Icon, label } = linkMap[link]
   return (
     <div
       className={cn(
         'group flex w-full items-center border-l-4 border-transparent py-2 hover:border-blue-300',
       )}
     >
-      <Link className="flex grow items-center" href={href}>
-        <div className="mr-3 h-6 w-6">{icon}</div>
-        <div className="grow text-base leading-5">{label}</div>
-      </Link>
+      <div className="mr-3 h-6 w-6">
+        <Icon width="100%" />
+      </div>
+      <div className="grow text-base leading-5">{label}</div>
     </div>
   )
 }
